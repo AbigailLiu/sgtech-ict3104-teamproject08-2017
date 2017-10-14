002a83cf75cee1a92d82cee839a058860c3b9dce
hweichou <weichou_93@hotmail.sg>
2017-10-14 20:07:18 +0800

    Add in missing unversioned files

---
+++ src/main/java/com/lifestyle/stps/algorithms/holder/RSEmail.java
@@ -0,0 +1,16 @@
+package com.lifestyle.stps.algorithms.holder;
+
+/**
+ * Created by User 1 on 13/10/2017.
+ */
+public class RSEmail {
+    private String email;
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+}
